2cd947e9e8a9f8b329af8e27aee06540e9b22174
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-17 16:10:15 +0200

    feat: add logics for the rest of modals

--- dist/script.js
+++ dist/script.js
@@ -13,23 +13,42 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 const modals = () => {
-  function bindModal(trigger, modal, close) {
-    trigger.addEventListener('click', e => {
-      if (e.target) {
-        e.preventDefault();
-      }
-      modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
+    trigger.forEach(item => {
+      item.addEventListener('click', e => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        modal.style.display = 'block';
+        //document.body.classList.add('modal-open');
+        document.body.style.overflow = 'hidden';
+      });
     });
     close.addEventListener('click', () => {
       modal.style.display = 'none';
+      //document.body.classList.remove('modal-open');
       document.body.style.overflow = '';
     });
+    modal.addEventListener('click', e => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+        //document.body.classList.remove('modal-open');
+        document.body.style.overflow = '';
+      }
+    });
   }
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn');
-  const modalEngineer = document.querySelector('.popup_engineer');
-  const modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+  function showModalByTime(selector, time) {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalByTime('.popup', 3000);
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
 
